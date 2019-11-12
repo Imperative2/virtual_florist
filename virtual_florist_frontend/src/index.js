070508abd85a2fd29129a71910bb397da45516e3
Imperative2 <nie335@gmail.com>
2019-11-12 11:46:09 +0100

    repaired deleted files

--- virtual_florist_frontend/src/index.js
+++ virtual_florist_frontend/src/index.js
@@ -12,10 +12,8 @@ import { Provider } from "react-redux";
 import wikiReducer from "./redux/reducers/wikiReducer";
 import productsReducer from "./redux/reducers/productsReducer";
 
-// import "@fortawesome/fontawesome-free/css/all.min.css";
-// import "bootstrap-css-only/css/bootstrap.min.css";
-// import "mdbreact/dist/css/mdb.css";
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
 const rootReducer = combineReducers({
